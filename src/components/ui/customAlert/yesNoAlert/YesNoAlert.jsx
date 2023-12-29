2fa87d37f03430fb79918edbeae0ec9fc30f9043
Siddharth Bhat <dbgt4531@gmail.com>
2023-12-29 16:36:08 +0530

    added components and beautified UI

---
+++ src/components/ui/customAlert/yesNoAlert/YesNoAlert.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './YesNoAlert.module.css';
+import { motion } from 'framer-motion';
+
+function YesNoAlert({ message, onClose, onSubmit }) {
+    return (
+        <div className={classes.customAlert}>
+            <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.1 }} className={classes.customAlertContent}>
+                <span className={classes.header}>{message.header}</span>
+                <div className={classes.bottomContent}>
+                    <motion.svg initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.3, ease: [0, 0.71, 0.2, 1.01], scale: { type: "spring", damping: 10, stiffness: 100, restDelta: 0.001 } }} xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                        <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                    </motion.svg>
+                    <span className={classes.submessage}>{message.submessage}</span>
+                    <div className={classes.yesNoButtons}>
+                        <button className={classes.customAlertOkButton} onClick={onSubmit}>Yes</button>
+                        <button className={classes.customAlertOkButton} onClick={onClose}>No</button>
+                    </div>
+                </div>
+            </motion.div>
+        </div>
+    );
+}
+
+export default YesNoAlert;
--- src/utils/Routers.jsx
+++ src/utils/Routers.jsx
@@ -6,6 +6,12 @@ import DashboardAdmin from '../components/admin/pages/dashboard/Dashboard';
 import LoginUser from '../components/user/pages/login/Login';
 import LayoutUser from '../components/user/layout/Layout';
 import DashboardUser from '../components/user/pages/dashboard/Dashboard';
+import Users from '../components/admin/pages/users/Users';
+import Media from '../components/admin/pages/media/Media';
+import Playlist from '../components/admin/pages/playlist/Playlist';
+import Category from '../components/admin/pages/category/Category';
+import Artist from '../components/admin/pages/artist/Artist';
+import Album from '../components/admin/pages/album/Album';
 
 const Routers = () => {
     return (
@@ -14,6 +20,12 @@ const Routers = () => {
                 <Route path='admin/login' element={<LoginAdmin />} />
                 <Route path='admin' element={<LayoutAdmin />}>
                     <Route index element={<DashboardAdmin />} />
+                    <Route path='users' element={<Users />} />
+                    <Route path='media' element={<Media />} />
+                    <Route path='playlist' element={<Playlist />} />
+                    <Route path='category' element={<Category />} />
+                    <Route path='artist' element={<Artist />} />
+                    <Route path='album' element={<Album />} />
                 </Route>
                 <Route path='login' element={<LoginUser />} />
                 <Route path='/' element={<LayoutUser />}>
