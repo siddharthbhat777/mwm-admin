03c8a88f4906933c58b66b1b310cf9237e1bf9cb
Siddharth Bhat <dbgt4531@gmail.com>
2024-01-02 10:39:27 +0530

    created basic table layout

--- src/components/admin/pages/users/Users.jsx
+++ src/components/admin/pages/users/Users.jsx
@@ -1,8 +1,220 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './Users.module.css';
+import Table from '../../../ui/table/Table';
 
-const Users = () => {
+const usersList = [
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Ruchi2024',
+        password: 'Ruchi2024',
+        firstname: 'Ruchita',
+        lastname: 'Ganacharya',
+        phone: 9765474787,
+        email: 'ruchi4gana@gmail.com',
+        institute: 'IOM',
+        program: 'MMS',
+        gr_number: '6745378523458',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    },
+    {
+        username: 'Sid2024',
+        password: 'Sid2024',
+        firstname: 'Siddharth',
+        lastname: 'Bhat',
+        phone: 8208465375,
+        email: 'dbgt4531@gmail.com',
+        institute: 'ICS',
+        program: 'MCA',
+        gr_number: '5875876765768',
+        year: '2024',
+        createdAt: '2024-01-01T11:19:37Z'
+    }
+];
+
+
+const Users = () => { // username, password, firstname, lastname, phone, email, institute, program, grno, year 
+    const [openDetails, setOpenDetails] = useState(false);
+
+    const columns = [
+        { field: 'firstname', header: 'Firstname' },
+        { field: 'lastname', header: 'Lastname' },
+        { field: 'email', header: 'E-Mail' },
+        { field: 'institute', header: 'Institute' },
+        { field: 'year', header: 'Year' },
+        { field: 'createdAt', header: 'Date', type: 'Date' }
+    ];
+
+    const handleSelectedRow = (row) => {
+        // setOpenCreate(false);
+        // setOpenDetails(true);
+        // setSelectedRowData(row);
+    };
     return (
-        <div>Users</div>
+        <div className={classes.outletScreen}>
+            <div></div>
+            <div className={classes.tableLayout}>
+                <div className={classes.mainLayout}>
+                    <Table rows={usersList} columns={columns} isRowSelected={openDetails} selectedRow={handleSelectedRow} />
+                </div>
+                <div className={classes.bottomLayout}>Bottom</div>
+            </div>
+        </div>
     );
 };
 
---
+++ src/components/ui/table/Table.jsx
@@ -0,0 +1,66 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import classes from './Table.module.css';
+
+const formatDate = (createdAt) => {
+    if (createdAt === null) {
+        return null;
+    }
+    const date = new Date(createdAt);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day < 10 ? `0${day}` : day}/${month < 10 ? `0${month}` : month}/${year}`;
+};
+
+const Table = ({ rows, columns, isRowSelected, selectedRow }) => {
+    const [selectedRowIndex, setSelectedRowIndex] = useState(null);
+
+    useEffect(() => {
+        if (!isRowSelected) {
+            setSelectedRowIndex(null);
+        }
+    }, [isRowSelected]);
+
+    const handleRowSelect = (row, index) => {
+        selectedRow(row);
+        setSelectedRowIndex(index);
+    };
+
+    return (
+        <Fragment>
+            {
+                rows.length !== 0 ?
+                    <table className={classes.mainTable}>
+                        <thead>
+                            <tr>
+                                {columns.map((column) => (
+                                    <th key={column.field}>{column.header}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                rows.map((row, index) => (
+                                    <tr key={row.id} onClick={() => handleRowSelect(row, index)} className={index === selectedRowIndex ? classes.selectedRow : ''}>
+                                        {
+                                            columns.map((column) => (
+                                                <td key={column.field}>
+                                                    {
+                                                        column.type === 'Date' ? formatDate(row[column.field]) : row[column.field]
+                                                    }
+                                                </td>
+                                            ))
+                                        }
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                    :
+                    <div className={classes.noData}>No data found</div>
+            }
+        </Fragment>
+    );
+};
+
+export default Table;
