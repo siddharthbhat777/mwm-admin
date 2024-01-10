77679418216d7ffb84e0e12c63b1486ccefd840f
Siddharth Bhat <dbgt4531@gmail.com>
2024-01-10 13:14:26 +0530

    added import button

--- src/components/admin/pages/users/Users.jsx
+++ src/components/admin/pages/users/Users.jsx
@@ -330,6 +330,10 @@ const Users = () => {
         setSelectedRowData(row);
     };
 
+    const handleImportData = () => {
+        console.log('Data imported');
+    };
+
     return (
         <div className={classes.fullScreen}>
             <AnimatePresence>
@@ -362,6 +366,14 @@ const Users = () => {
                                 </div>
                                 <input type='text' placeholder='Search here' className={classes.searchInput} onChange={(e) => setSearchQuery(e.target.value)} />
                             </div>
+                            <button className={classes.topCreateButton} onClick={handleImportData}>
+                                <div className={classes.createButtonContainer}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+                                        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0" />
+                                    </svg>
+                                    <span>Import</span>
+                                </div>
+                            </button>
                         </div>
                         <hr className={classes.tableDivideLine} />
                         <div className={classes.tableContainer}>
