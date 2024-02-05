f92dcd5baa87f14700af6425b85286378bdcfb34
Siddharth Bhat <dbgt4531@gmail.com>
2024-02-05 10:20:36 +0530

    modified update ui

--- src/components/admin/pages/users/Users.jsx
+++ src/components/admin/pages/users/Users.jsx
@@ -359,6 +359,15 @@ const DetailsView = ({ setOpenDetails, detailsData }) => {
         setYear(value);
     };
 
+    const handleEditChanges = async () => {
+        try {
+            await axios.put(`http://localhost:5000/api/auth/update-user/`)
+            setEditMode(false);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <motion.div initial={{ width: '0' }} animate={window.innerWidth > 480 ? { width: '60%' } : { width: '100%' }} exit={{ width: '0' }} transition={{ duration: 0.2 }} className={classes.detailsViewContainer}>
             <div className={classes.detailsContainer}>
@@ -418,7 +427,7 @@ const DetailsView = ({ setOpenDetails, detailsData }) => {
                         <header className={classes.detailsData}>Year: </header>
                         {
                             editMode ?
-                            <CustomDropdown defaultText={detailsData.year} options={yearsDropdownOptions} onSelect={handleYearSelect} />
+                                <CustomDropdown defaultText={detailsData.year} options={yearsDropdownOptions} onSelect={handleYearSelect} />
                                 :
                                 <data>{detailsData.year}</data>
                         }
@@ -434,19 +443,14 @@ const DetailsView = ({ setOpenDetails, detailsData }) => {
                     </div>
                     <div className={classes.flexBr}>
                         <header className={classes.detailsData}>Status: </header>
-                        {
-                            editMode ?
-                                <input type='text' className={`${classes.formInput} ${classes.smallerInputSize}`} placeholder='Status' defaultValue={detailsData.status} />
-                                :
-                                <data>{detailsData.status}</data>
-                        }
+                        <data>{detailsData.status === 1? 'Active' : 'Inactive'}</data>
                     </div>
                 </motion.div>
                 <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.1 }} className={classes.modifyButtons}>
                     {
                         editMode ?
                             <Fragment>
-                                <button className={classes.editButton} onClick={() => setEditMode(true)}>Save changes &nbsp;
+                                <button className={classes.editButton} onClick={handleEditChanges}>Save changes &nbsp;
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-floppy2" viewBox="0 0 16 16">
                                         <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v3.5A1.5 1.5 0 0 1 11.5 6h-7A1.5 1.5 0 0 1 3 4.5V1H1.5a.5.5 0 0 0-.5.5m9.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
                                     </svg>
